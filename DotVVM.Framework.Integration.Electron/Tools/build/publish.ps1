070783d4fd0fe8f39f856bb42df8ea44cd66a34b
Tomas Jurasek <tomas.jurasek@riganti.cz>
2017-09-28 08:05:37 +0200

    updated script

--- DotVVM.Framework.Integration.Electron/Tools/build/publish.ps1
+++ DotVVM.Framework.Integration.Electron/Tools/build/publish.ps1
@@ -31,15 +31,13 @@ $LASTEXITCODE
 }
 
 function CleanOldGeneratedPackages() {
-    If (Test-Path "./.nupkgs"){
-	    Remove-Item "./.nupkgs"
-    }
+    Remove-Item "**/.nupkgs"
 }
 
 
 function SetVersion() {
   	foreach ($package in $packages) {
-        $filePath = ".\$($package.Directory).csproj"
+        $filePath = ".\$($package.Directory)\$($package.Package).csproj"
         $file = [System.IO.File]::ReadAllText($filePath, [System.Text.Encoding]::UTF8)
 		$file = [System.Text.RegularExpressions.Regex]::Replace($file, "\<VersionPrefix\>([^<]+)\</VersionPrefix\>", "<VersionPrefix>" + $version + "</VersionPrefix>")
 		$file = [System.Text.RegularExpressions.Regex]::Replace($file, "\<PackageVersion\>([^<]+)\</PackageVersion\>", "<PackageVersion>" + $version + "</PackageVersion>")
